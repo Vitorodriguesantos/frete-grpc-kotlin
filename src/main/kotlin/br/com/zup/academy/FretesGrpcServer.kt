86236ec3417701c07cc23d075b316359e98d7a88
Vitor Rodrigues <vitorodriguesantos@hotmail.com>
2021-07-07 14:36:04 -0300

    adicionando validacoes grpc

--- src/main/kotlin/br/com/zup/academy/FretesGrpcServer.kt
+++ src/main/kotlin/br/com/zup/academy/FretesGrpcServer.kt
@@ -1,7 +1,13 @@
 package br.com.zup.academy
 
+import com.google.protobuf.Any
+import com.google.rpc.Code
+import com.google.rpc.StatusProto
+import io.grpc.Status
+import io.grpc.protobuf.StatusProto.*
 import io.grpc.stub.StreamObserver
 import org.slf4j.LoggerFactory
+import java.lang.IllegalStateException
 import javax.inject.Singleton
 import kotlin.random.Random
 
@@ -14,9 +20,60 @@ class FretesGrpcServer : FretesServiceGrpc.FretesServiceImplBase(){
 
         logger.info("Calculando frete para request ${request}")
 
+        //logica para validar se cep esta preenchido
+        if(request!!.cep == null || request!!.cep.isBlank()){
+            val e = Status.INVALID_ARGUMENT
+                        .withDescription("cep deve ser informado")
+                        .asRuntimeException()
+
+            responseObserver?.onError(e)
+        }
+
+        //simulando erro com acesso negado
+        if(request.cep.endsWith("333")){
+
+            val acessoNegado = com.google.rpc.Status.newBuilder()
+                        .setCode(Code.PERMISSION_DENIED.number)
+                        .setMessage("usuario nao pode acessar este recurso")
+                        .addDetails(Any.pack(ErroDetails.newBuilder()
+                            .setCode(401)
+                            .setMessage("Token expirado")
+                            .build()))
+                        .build()
+
+           val e = toStatusRuntimeException(acessoNegado)//convertendo o erro para runtimeexception
+            responseObserver?.onError(e)
+        }
+
+        //logica para verificar formato do cep
+        if(!request.cep.matches("[0-9]{5}-[0-9]{3}".toRegex())){
+            val e = Status.INVALID_ARGUMENT
+                        .withDescription("cep no formato invalido")
+                        .augmentDescription("forma esperado 00000-000")
+                        .asRuntimeException()
+
+            responseObserver?.onError(e)
+        }
+
+        var valor = 0.0
+
+        //simulando erro com regra de negocio
+        try {
+            valor = Random.nextDouble(0.0,100.0)
+            if (valor < 70){
+                throw IllegalStateException("Erro executar regra de negocio")
+            }
+        }catch (e: Exception){
+            responseObserver?.onError(Status.INTERNAL
+                .withDescription(e.message)
+                .withCause(e)
+                .asRuntimeException())
+
+        }
+
         val response = CalculaFreteResponse.newBuilder()
             .setCep(request!!.cep)
-            .setValor(Random.nextDouble(0.0,100.0))
+            .setValor(valor)
             .build()
 
         logger.info("Frete calculado ${response}")
